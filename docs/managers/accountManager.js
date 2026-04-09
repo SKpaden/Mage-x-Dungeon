eb2d63727472c78501f6e43924b2f3e5fb677c66
SKpaden <finn-luca.nannt@gmx.de>
2026-04-09 21:19:44 +0200

    Moved all code to root level for better hosting with Github pages.

--- docs/managers/accountManager.js
+++
@@ -1,6 +0,0 @@
-import { updateStageData } from "./stageManager.js";
-
-export function updateStageAccountData(scene){
-    // maybe do more here later if I have an Account class with a stages field
-    updateStageData(scene);
-}
