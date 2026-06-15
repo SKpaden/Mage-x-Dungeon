bf9dcdf6bfe24104121971c60f1cb0a6e3dff712
SKpaden <finn-luca.nannt@gmx.de>
2026-06-15 18:39:08 +0200

    Adjusted SkillContext.

--- data/skillContext.js
+++ data/skillContext.js
@@ -1,11 +1,24 @@
 export class SkillContext {
-    constructor(scene, source, target, index, allies, enemies) {
+    constructor(scene, source, target, index, allies, enemies, affectedTargets = null) {
         this.scene = scene;
         this.source = source;
         this.target = target;
         this.index = index,
         this.allies = allies;
         this.enemies = enemies;
+
+        this.affectedTargets = affectedTargets ? affectedTargets : [index];
+        this.currentTarget = null;  // events need to process current targets, not just the original target
+
+        // Pipelines might add these flags / fields:
+        // - blocked
+        // - death
+        // - debuff
+        // - dmg
+        // - element
+        // - logQueueKey
+        // - modifiedDamage
+        // - type
 
         this.results = {
             damageDealt: new Map(),   // targetId → damage
