044949f6fc973c10bd700ade20ae33a731af0766
SKpaden <finn-luca.nannt@gmx.de>
2026-07-31 16:53:02 +0200

    Added helper functions to get and set stats via StatManager rather than just the container.

--- data/statManager.js
+++ data/statManager.js
@@ -1,9 +1,15 @@
+import { gameState } from "../game/gameState.js";
+
 export class StatManager{
     constructor(stats){
         this.statData = stats;
     }
 
-    // Copies stats to not mutate later.
+    /**
+     * Creates and returns a deep copy of a stats object.
+     * @param {Object} stats The stats object
+     * @returns {Object} The copied stats object
+     */
     static copyStats(stats){
         return {
         'speed': {
@@ -64,6 +70,37 @@ export class StatManager{
     //         }
     //     }
 
+    /**
+     * Gets the current/base stat for a Character stored in a Phaser container object.
+     * @param {Object} container The Phaser container belonging to the Character
+     * @param {String} key The name of the stat to look for
+     * @param {boolean} current Whether to get current or base stat
+     * @returns {number} The current/base stat for the Character
+     */
+    static getContainerStat(container, key, current = true){
+        const char = container.getData("char");
+        const statManager = char.statManager;
+        return current ? statManager.getCurrentStat(key) : statManager.getBaseStat(key);
+    }
+
+    /**
+     * Updates the value of a stat for a Character stored in a Phaser container object.
+     * @param {Object} container The Phaser container belonging to the Character
+     * @param {String} key The name of the stat to update
+     * @param {number} newValue The new value for the current stat
+     */
+    static setContainerStat(container, key, newValue){
+        const char = container.getData("char");
+        const statManager = char.statManager;
+        const oldVal = statManager.getCurrentStat(key);
+        statManager.setCurrentStat(key, newValue);
+        if (key === 'speed') gameState.combinedSpeed += newValue - oldVal;
+    }
+
+    /**
+     * Gets all current stats for a Character
+     * @returns {Object} All current stats of a Character
+     */
     getAllCurrentStats() {
         const result = {};
         for (const [key, value] of Object.entries(this.statData)) {
@@ -79,26 +116,56 @@ export class StatManager{
         return result;
     }
 
+    /**
+     * Gets the base resistance for an element.
+     * @param {String} elementName The name of the element
+     * @returns {float} The resistance value
+     */
     getBaseResistance(elementName){
         return this.statData.resistances?.[elementName]?.base ?? 1.0;
     }
 
+    /**
+     * Gets the base value for a stat.
+     * @param {String} statName The name for the stat to get
+     * @returns {number} The base value for the stat
+     */
     getBaseStat(statName){
         return this.statData[statName].base;
     }
 
+    /**
+     * Gets the current resistance value for an element.
+     * @param {String} elementName The name for the element
+     * @returns {number} The current resistance value
+     */
     getCurrentResistance(elementName){
         return this.statData.resistances?.[elementName]?.current ?? 1.0;  // safe accessing
     }
 
+    /**
+     * Gets the current value for a stat.
+     * @param {String} statName The name for the stat to get
+     * @returns {number} The current value for the stat
+     */
     getCurrentStat(statName){
         return this.statData[statName].current;
     }
 
+    /**
+     * Updates the current resistance for an element.
+     * @param {String} elementName The name of the element
+     * @param {float} newValue The new value for the resistance
+     */
     setCurrentResistance(elementName, newValue){
         this.statData.resistances[elementName].current = newValue;
     }
 
+    /**
+     * Updates the current value of a stat.
+     * @param {String} statName The stat name
+     * @param {number} newValue The new value for the stat
+     */
     setCurrentStat(statName, newValue){
         this.statData[statName].current = newValue;
     }
